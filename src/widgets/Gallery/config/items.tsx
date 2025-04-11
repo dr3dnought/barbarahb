3391188b67a1902f16abac1261546f8cf54a2128
dr3dnought <sheremetyev48@mail.ru>
2025-04-11 17:08:09 +0300

    [Feat] Bob

--- src/widgets/Gallery/config/items.tsx
+++ src/widgets/Gallery/config/items.tsx
@@ -5,12 +5,11 @@ export const items: CardProps[] = [
     {
         src: '/assets/gallery-photo-01.jpg',
 
-        title: 'Текст',
+        title: 'Варвара!',
         description: (
             <p>
-                Cursus aenean, dictum nascetur eleifend et tortor aliquam elit, dolor. Aliquam condimentum lacus
-                praesent quis vel, cras nisi nunc consectetur. Vivamus est ut ut hac suscipit dolor fermentum dapibus
-                tortor.
+                Спешу поздравить тебя с 21-ым днем рождения! Этот подарок на самом деле я делал дольше чем один день,
+                но я хотел сделать его максимально качественным. Ты часто спрашивала меня, почему я тебя люблю, так вот, листай карточки, находи ответы!
             </p>
         ),
 
@@ -20,8 +19,12 @@ export const items: CardProps[] = [
     {
         src: '/assets/gallery-photo-02.jpg',
 
-        title: '',
-        description: '',
+        title: 'Красота',
+        description: (
+            <p>
+                Прототипом Канделла на м2 была ты, Варвара! You fill ma live with only bright colors. Ты мой источник Витамина D, твоя внешность слишком яркая и запоминающаяся, я не могу не смотреть на тебя даже когда злой.
+            </p>
+        ),
 
         hueA: 14.51,
         hueB: 21.57,
@@ -29,8 +32,12 @@ export const items: CardProps[] = [
     {
         src: '/assets/gallery-photo-03.jpg',
 
-        title: '',
-        description: '',
+        title: 'Забота',
+        description: (
+            <p>
+                Я это чувствую везде, от твоего "Как ты?" до ощущения колебаний твоих мыслей в моем мозге, иначе мое 7 чувство я объяснить не могу
+            </p>
+        ),
 
         hueA: 28.63,
         hueB: 36.08,
@@ -38,8 +45,12 @@ export const items: CardProps[] = [
     {
         src: '/assets/gallery-photo-04.jpg',
 
-        title: '',
-        description: '',
+        title: 'Ma controller',
+        description: (
+            <p>
+                Потому что ты показываешь пример: уметь любить других, не забывая о себе.
+            </p>
+        ),
 
         hueA: 43.14,
         hueB: 50.59,
@@ -47,8 +58,12 @@ export const items: CardProps[] = [
     {
         src: '/assets/gallery-photo-05.jpg',
 
-        title: '',
-        description: '',
+        title: 'Домовитость',
+        description: (
+            <p>
+                Где бы мы ни были, с тобой я чувствую себя на месте
+            </p>
+        ),
 
         hueA: 57.65,
         hueB: 64.71,
@@ -56,8 +71,12 @@ export const items: CardProps[] = [
     {
         src: '/assets/gallery-photo-06.jpg',
 
-        title: '',
-        description: '',
+        title: 'Мы разные, но...',
+        description: (
+            <p>
+                мы от этого не становимся дальше друг от друга, ты так не считаешь, но то, что мы меняемся как вместе, так и по отдельности, меня лично вдохновляет
+            </p>
+        ),
 
         hueA: 72.16,
         hueB: 79.22,
@@ -65,8 +84,12 @@ export const items: CardProps[] = [
     {
         src: '/assets/gallery-photo-07.jpg',
 
-        title: '',
-        description: '',
+        title: 'Ты умеешь любить',
+        description: (
+            <p>
+                Я не знаю, как и в чем это проявляется, но я чувствую это, и я очень рад, что ты делаешь это именно так, экспромтом, не задумываясь, не думая, просто делая
+            </p>
+        ),
 
         hueA: 86.27,
         hueB: 93.73,
@@ -74,8 +97,12 @@ export const items: CardProps[] = [
     {
         src: '/assets/gallery-photo-08.jpg',
 
-        title: '',
-        description: '',
+        title: 'Твоя харизма',
+        description: (
+            <p>
+                "Ее люблю не только я, но и все мои друзья, и не только мои" Оставайся таким же социальным магнитом
+            </p>
+        ),
 
         hueA: 100.78,
         hueB: 107.84,
@@ -83,8 +110,12 @@ export const items: CardProps[] = [
     {
         src: '/assets/gallery-photo-09.jpg',
 
-        title: '',
-        description: '',
+        title: 'Твоя честность',
+        description: (
+            <p>
+                Даже если правда неудобная, я знаю: ты скажешь прямо, не сразу конечно, но прямо
+            </p>
+        ),
 
         hueA: 115.29,
         hueB: 122.35,
@@ -92,17 +123,24 @@ export const items: CardProps[] = [
     {
         src: '/assets/gallery-photo-10.jpg',
 
-        title: '',
-        description: '',
+        title: 'Просто потому что ты, это ты',
+        description: (
+            <p>
+                Есть вещи которые я не могу объяснить, но я люблю тебя просто потому что ты, это ты
+            </p>
+        ),
 
         hueA: 129.41,
         hueB: 136.86,
     },
     {
         src: '/assets/gallery-photo-11.jpg',
-
-        title: '',
-        description: '',
+        title: 'Спасибо',
+        description: (
+            <p>
+               U are the best part of ma live...
+            </p>
+        ),
 
         hueA: 143.92,
         hueB: 151.37,
@@ -110,24 +148,14 @@ export const items: CardProps[] = [
     {
         src: '/assets/gallery-photo-12.jpg',
 
-        title: '',
-        description: '',
+        title: 'Хомачки!',
+        description: (
+            <p>
+                Тоже пришли тебя поздравить!
+            </p>
+        ),
 
         hueA: 158.43,
         hueB: 165.49,
-    },
-    {
-        cover: <h3 className={'font-poppins text-8xl text-stone-500'}>?</h3>,
-
-        title: 'Ой',
-        description: (
-            <span className={'space-y-4'}>
-                <p>Совсем забыл упоменуть. У меня для тебя есть подарок</p>
-                <LinkButton href={'/game'}>Какой?</LinkButton>
-            </span>
-        ),
-
-        hueA: 172.94,
-        hueB: 180,
     },
 ];
